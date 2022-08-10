885592daa0292ace3c26b58c0dff85e95e4ab970
JustLittle22 <nhatanhd96@gmail.com>
2022-08-10 09:43:34 +0700

    Ve hinh tron

---
+++ Vehinhtron.py
@@ -0,0 +1,21 @@
+from tkinter import ANCHOR
+import turtle
+
+anh_screen = turtle.Screen()
+
+#Tạo cây bút vẽ
+cay_but = turtle.Turtle()
+cay_but.pensize(5)
+
+#Tô màu viền và trong của hình tròn
+cay_but.pencolor("red")
+cay_but.fillcolor("blue")
+cay_but.begin_fill()
+
+#Vẽ hình tròn
+cay_but.circle(100)
+cay_but.end_fill()
+
+
+
+turtle.done()
